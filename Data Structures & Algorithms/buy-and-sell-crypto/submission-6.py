2546adcda60848c8c815fc8cf4ffd31102e11abb
Shiori <cindyshiori.k@gmail.com>
2026-09-22 15:11:16 -0500

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        時間計算量: O(n)
+        空間計算量: O(1)
+        """
+        l, r = 0, 1
+        profit = 0
+
+        while r < len(prices):
+            if prices[r] < prices[l]:
+                l = r
+            else:
+                curr_profit = prices[r] - prices[l]
+                profit = max(profit, curr_profit)
+            r += 1
+
+        return profit
